7bb41b2de16c8df192d401aef2a7b650dd7d6127
Matthias Jost <8178852+matthiasjost@users.noreply.github.com>
2022-12-03 09:10:12 +0100

    Day 2

---
+++ AdventOfCode2022/Day2RockPaperScissor/Program.cs
@@ -0,0 +1,23 @@
+﻿using Day2RockPaperScissor;
+
+namespace Day2RockPaperScissor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read entire text file content in one string  
+            string[] text = File.ReadAllLines("input.txt");
+
+
+            var calculator = new SituationOutcomeType.RockPaperScissorsScoreCalculator();
+            foreach (string line in text)
+            {
+                string[] columns = line.Split(' ');
+                calculator.AddRound(columns[1], columns[0]);
+            }
+
+            Console.WriteLine(calculator.CalculateRounds());
+        }
+    }
+}
---
+++ AdventOfCode2022/Day2RockPaperScissor/RockPaperScissorsScoreCalculator.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2RockPaperScissor
+{
+    public class SituationOutcomeType
+    {
+        public enum RockPaperScissor
+        {
+            Rock = 1,
+            Paper = 2,
+            Scissor = 3,
+        }
+        public enum SituationScoreType
+        {
+            Loss = 0,
+            Draw = 3,
+            Won = 6
+        }
+        public RockPaperScissor Player1 { get; set; }
+        public RockPaperScissor Player2 { get; set; }
+        public SituationScoreType SituationPlayer1Score { get; set; }
+        public SituationScoreType SituationPlayer2Score
+        {
+            get
+            {
+                switch (SituationPlayer1Score)
+                {
+                    case SituationScoreType.Loss:
+                        return SituationScoreType.Won;
+                    case SituationScoreType.Won:
+                        return SituationScoreType.Loss;
+                    case SituationScoreType.Draw:
+                        return SituationScoreType.Draw;
+                    default:
+                        return SituationScoreType.Loss;
+                }
+            }
+        }
+
+        public class Round
+        {
+            public RockPaperScissor Player1 { get; set; }
+            public RockPaperScissor Player2 { get; set; }
+            public int ScorePlayer1 { get; set; }
+        }
+
+        public class RockPaperScissorsScoreCalculator
+        {
+            private List<SituationOutcomeType> _possibleOutcomes = new List<SituationOutcomeType>();
+            private List<Round> _roundList { get; set; }
+
+            public RockPaperScissorsScoreCalculator()
+            {
+                _roundList = new List<Round>();
+                InitOutcome();
+            }
+            private void InitOutcome()
+            {
+                _possibleOutcomes = new List<SituationOutcomeType>();
+                _possibleOutcomes.Add(new SituationOutcomeType()
+                {
+                    Player1 = RockPaperScissor.Rock,
+                    Player2 = RockPaperScissor.Rock,
+                    SituationPlayer1Score = SituationScoreType.Draw
+                });
+                _possibleOutcomes.Add(new SituationOutcomeType()
+                {
+                    Player1 = RockPaperScissor.Rock,
+                    Player2 = RockPaperScissor.Paper,
+                    SituationPlayer1Score = SituationScoreType.Loss
+                });
+                _possibleOutcomes.Add(new SituationOutcomeType()
+                {
+                    Player1 = RockPaperScissor.Rock,
+                    Player2 = RockPaperScissor.Scissor,
+                    SituationPlayer1Score = SituationScoreType.Won
+                });
+
+
+                _possibleOutcomes.Add(new SituationOutcomeType()
+                {
+                    Player1 = RockPaperScissor.Paper,
+                    Player2 = RockPaperScissor.Rock,
+                    SituationPlayer1Score = SituationScoreType.Won
+                });
+                _possibleOutcomes.Add(new SituationOutcomeType()
+                {
+                    Player1 = RockPaperScissor.Paper,
+                    Player2 = RockPaperScissor.Paper,
+                    SituationPlayer1Score = SituationScoreType.Draw
+                });
+                _possibleOutcomes.Add(new SituationOutcomeType()
+                {
+                    Player1 = RockPaperScissor.Paper,
+                    Player2 = RockPaperScissor.Scissor,
+                    SituationPlayer1Score = SituationScoreType.Loss
+                });
+
+
+                _possibleOutcomes.Add(new SituationOutcomeType()
+                {
+                    Player1 = RockPaperScissor.Scissor,
+                    Player2 = RockPaperScissor.Rock,
+                    SituationPlayer1Score = SituationScoreType.Loss
+                });
+                _possibleOutcomes.Add(new SituationOutcomeType()
+                {
+                    Player1 = RockPaperScissor.Scissor,
+                    Player2 = RockPaperScissor.Paper,
+                    SituationPlayer1Score = SituationScoreType.Won
+                });
+                _possibleOutcomes.Add(new SituationOutcomeType()
+                {
+                    Player1 = RockPaperScissor.Scissor,
+                    Player2 = RockPaperScissor.Scissor,
+                    SituationPlayer1Score = SituationScoreType.Draw
+                });
+            }
+
+            public int CalculateRounds()
+            {
+                foreach (Round roundItem in _roundList)
+                {
+                    var outcome = _possibleOutcomes
+                        .First(s => s.Player1 == roundItem.Player1 && s.Player2 == roundItem.Player2);
+
+                    roundItem.ScorePlayer1 = (int)(outcome.SituationPlayer1Score + (int)roundItem.Player1);
+                }
+
+                return _roundList.Sum(r => r.ScorePlayer1);
+            }
+            public void AddRound(string player1Input, string player2Input)
+            {
+                RockPaperScissor player1Type = RockPaperScissor.Rock;
+                RockPaperScissor player2Type = RockPaperScissor.Rock;
+
+                switch (player1Input)
+                {
+                    case "A":
+                        {
+                            player1Type = RockPaperScissor.Rock;
+                            break;
+                        }
+                    case "B":
+                        {
+                            player1Type = RockPaperScissor.Paper;
+                            break;
+                        }
+                    case "C":
+                        {
+                            player1Type = RockPaperScissor.Scissor;
+                            break;
+                        }
+                }
+
+                switch (player2Input)
+                {
+                    case "X":
+                        {
+                            player2Type = RockPaperScissor.Rock;
+                            break;
+                        }
+                    case "Y":
+                        {
+                            player2Type = RockPaperScissor.Paper;
+                            break;
+                        }
+                    case "Z":
+                        {
+                            player2Type = RockPaperScissor.Scissor;
+                            break;
+                        }
+                }
+                _roundList.Add(new Round() { Player1 = player1Type, Player2 = player2Type });
+            }
+        }
+    }
+}
